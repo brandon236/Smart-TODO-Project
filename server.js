4e8357c58062504d6c9561d0c3a29631c57733a5
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-27 22:28:22 +0000

    Add database

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
+const pool = new Pool({
+  user: ENV.DB_USER,
+  password: ENV.DB_PASS,
+  host: ENV.DB_PASS,
+  database: ENV.DB_NAME
+});
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
