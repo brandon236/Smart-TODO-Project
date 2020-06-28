f2747dc62074d29b7c654d710434a63daf2fc9c2
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-28 07:48:26 +0000

    API for our table working. E.g /api/users

--- server.js
+++ server.js
@@ -36,12 +36,15 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const listRoutes = require("./routes/list");
+const categoriesRoutes = require("./routes/categories");
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/categories", categoriesRoutes(db));
+app.use("/api/list", listRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -51,6 +54,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
